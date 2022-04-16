cd01865b509f4d35ba29cb9711bf4177b81b6010
Alex <htmldav@mail.ru>
2022-04-16 08:34:22 +0300

    import java.io.InputStream;

--- src/com/shephertz/app42/paas/sample/util/Util.java
+++ src/com/shephertz/app42/paas/sample/util/Util.java
@@ -2,6 +2,7 @@ package com.shephertz.app42.paas.sample.util;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
