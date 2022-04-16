0be4b16df5d29dff3439f33dcd27bd1a26e1d185
Alex <htmldav@mail.ru>
2022-04-16 09:03:41 +0300

    import java.io.*;

--- src/com/shephertz/app42/paas/sample/util/Util.java
+++ src/com/shephertz/app42/paas/sample/util/Util.java
@@ -1,11 +1,12 @@
 package com.shephertz.app42.paas.sample.util;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
+// import java.io.File;
+// import java.io.FileInputStream;
+// import java.io.InputStream;
 // import java.io.InputStreamReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+// import java.io.FileNotFoundException;
+// import java.io.IOException;
+import java.io.*;
 import java.util.Properties;
 
 public class Util {
